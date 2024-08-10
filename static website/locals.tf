4b7ecb70f988412e5c6491fb2d0ec30ade9ad1b3
Alves0611 <g.alves0611@gmail.com>
2024-08-10 15:43:33 -0300

    feat: add distribution_id to local variables for CloudFront

--- static website/locals.tf
+++ static website/locals.tf
@@ -4,7 +4,10 @@ locals {
   domain_name     = local.has_domain ? var.domain_name : random_pet.website.id
   regional_domain = "${local.domain_name}.s3.${var.aws_region}.amazonaws.com"
 
+  distribution_id = "E2F16ALWFZXHBJ"
+
   common_tags = {
+    "Module"    = "Static website"
     "ManagedBy" = "Terraform"
   }
 }
