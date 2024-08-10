d384e2a58745055df9101eae82d81dd6f31ab4e5
Alves0611 <g.alves0611@gmail.com>
2024-08-10 15:34:06 -0300

    feat: add ACM certificate validation resource using Route 53 records

--- static website/acm.tf
+++ static website/acm.tf
@@ -7,3 +7,12 @@ resource "aws_acm_certificate" "this" {
   validation_method         = "DNS"
   subject_alternative_names = ["*.${local.domain_name}"]
 }
+
+resource "aws_acm_certificate_validation" "this" {
+  count = local.has_domain ? 1 : 0
+
+  provider = aws.us_east_1
+
+  certificate_arn         = aws_acm_certificate.this[0].arn
+  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
+}
