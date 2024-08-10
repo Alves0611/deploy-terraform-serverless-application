4b88dfaffea4cd3337d5e0aab6624419f5070fc8
Alves0611 <g.alves0611@gmail.com>
2024-08-10 16:38:12 -0300

    feat: add local variables for account ID, namespaced service name, and domain presence check

--- serverless app/locals.tf
+++ serverless app/locals.tf
@@ -1,4 +1,9 @@
 locals {
+  account_id              = data.aws_caller_identity.this.account_id
+  namespaced_service_name = "${var.service_name}-${var.environment}"
+  has_domain_name         = var.domain_name != null
+
+
   common_tags = {
     "Module"    = "Static website"
     "ManagedBy" = "Terraform"
