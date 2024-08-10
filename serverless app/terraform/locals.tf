ef5a4eea14438d215542a7e535ebe88a076717ab
Alves0611 <g.alves0611@gmail.com>
2024-08-10 17:56:56 -0300

    refactor: reorganize Terraform files into 'terraform' directory

---
+++ serverless app/terraform/locals.tf
@@ -0,0 +1,19 @@
+locals {
+  account_id              = data.aws_caller_identity.this.account_id
+  namespaced_service_name = "${var.service_name}-${var.environment}"
+
+  has_domain_name = var.domain_name != null
+
+  dynamodb_config = {
+    partition_key = "TodoId"
+    sort_key      = "UserId"
+    gsi_name      = "TodosByUser"
+    gsi_sort_key  = "Done"
+  }
+
+
+  common_tags = {
+    "Module"    = "Static website"
+    "ManagedBy" = "Terraform"
+  }
+}
