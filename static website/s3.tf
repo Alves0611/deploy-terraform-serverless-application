70d998c8aed118baeea799a97f2333af911d056c
Alves0611 <g.alves0611@gmail.com>
2024-08-10 15:34:45 -0300

    feat: update S3 bucket policy to allow CloudFront access

--- static website/s3.tf
+++ static website/s3.tf
@@ -2,5 +2,28 @@ module "website_bucket" {
   source = "github.com/Alves0611/terraform-aws-s3-bucket"
 
   name     = local.domain_name
-  filepath = "${path.module}/../website/dist"
+  filepath = "${path.module}/website/dist"
+
+  policy = {
+    json = jsonencode({
+      "Version" : "2008-10-17",
+      "Id" : "PolicyForCloudFrontPrivateContent",
+      "Statement" : [
+        {
+          "Sid" : "AllowCloudFrontServicePrincipal",
+          "Effect" : "Allow",
+          "Principal" : {
+            "Service" : "cloudfront.amazonaws.com"
+          },
+          "Action" : "s3:GetObject",
+          "Resource" : "arn:aws:s3:::${local.domain_name}/*",
+          "Condition" : {
+            "StringEquals" : {
+              "AWS:SourceArn" : "arn:aws:cloudfront::${local.account_id}:distribution/${local.distribution_id}"
+            }
+          }
+        }
+      ]
+    })
+  }
 }
