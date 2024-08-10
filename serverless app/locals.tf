e93300ad688c0be3597296c4b09147e348345fe5
Alves0611 <g.alves0611@gmail.com>
2024-08-10 17:02:44 -0300

    chore: add DynamoDB configuration to local variables

--- serverless app/locals.tf
+++ serverless app/locals.tf
@@ -1,7 +1,15 @@
 locals {
   account_id              = data.aws_caller_identity.this.account_id
   namespaced_service_name = "${var.service_name}-${var.environment}"
-  has_domain_name         = var.domain_name != null
+
+  has_domain_name = var.domain_name != null
+
+  dynamodb_config = {
+    partition_key = "TodoId"
+    sort_key      = "UserId"
+    gsi_name      = "TodosByUser"
+    gsi_sort_key  = "Done"
+  }
 
 
   common_tags = {
