97b5bce6fc2ddeb70719c13af122efaf1473be39
Alves0611 <g.alves0611@gmail.com>
2024-08-11 12:32:22 -0300

    feat: add API Gateway deployment resource with conditional redeployment trigger

--- serverless app/terraform/api.tf
+++ serverless app/terraform/api.tf
@@ -72,3 +72,39 @@ resource "aws_api_gateway_integration" "todo" {
     "integration.request.path.todoId" = "method.request.path.todoId"
   }
 }
+
+resource "aws_api_gateway_deployment" "this" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+
+  triggers = {
+    # NOTE: The configuration below will satisfy ordering considerations,
+    #       but not pick up all future REST API changes. More advanced patterns
+    #       are possible, such as using the filesha1() function against the
+    #       Terraform configuration file(s) or removing the .id references to
+    #       calculate a hash against whole resources. Be aware that using whole
+    #       resources will show a difference after the initial implementation.
+    #       It will stabilize to only change when resources change afterwards.
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_resource.todos,
+      aws_api_gateway_resource.todo,
+      aws_api_gateway_method.todos,
+      aws_api_gateway_method.todo,
+      aws_api_gateway_integration.todos,
+      aws_api_gateway_integration.todo,
+      aws_api_gateway_method.cors,
+      aws_api_gateway_integration.cors,
+      aws_api_gateway_method_response.cors,
+      aws_api_gateway_integration_response.cors,
+      aws_api_gateway_method.cors_todo,
+      aws_api_gateway_integration.cors_todo,
+      aws_api_gateway_method_response.cors_todo,
+      aws_api_gateway_integration_response.cors_todo,
+      aws_api_gateway_gateway_response.cors_4xx,
+      aws_api_gateway_gateway_response.cors_5xx,
+    ]))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
