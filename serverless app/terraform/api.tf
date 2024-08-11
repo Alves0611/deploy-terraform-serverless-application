87c6169135a585172dc36360ece7025fa47cc39f
Alves0611 <g.alves0611@gmail.com>
2024-08-11 12:35:17 -0300

    feat: add CORS support for 'todo/{todoId}' resource in API Gateway

--- serverless app/terraform/api.tf
+++ serverless app/terraform/api.tf
@@ -211,3 +211,21 @@ resource "aws_api_gateway_integration_response" "cors" {
 
   depends_on = [aws_api_gateway_integration.cors]
 }
+
+resource "aws_api_gateway_method" "cors_todo" {
+  rest_api_id   = aws_api_gateway_rest_api.this.id
+  resource_id   = aws_api_gateway_resource.todo.id
+  authorization = "NONE"
+  http_method   = "OPTIONS"
+}
+
+resource "aws_api_gateway_integration" "cors_todo" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  resource_id = aws_api_gateway_resource.todo.id
+  http_method = aws_api_gateway_method.cors_todo.http_method
+  type        = "MOCK"
+
+  request_templates = {
+    "application/json" = "{\"statusCode\": 200}"
+  }
+}
