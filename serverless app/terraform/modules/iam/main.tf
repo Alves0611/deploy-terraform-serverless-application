10bdb5f1b9250e5a880c3b45c404531a0dcbd6d1
Alves0611 <g.alves0611@gmail.com>
2024-08-10 22:19:42 -0300

    feat: add conditional IAM policy statements for Lambda log permissions

--- serverless app/terraform/modules/iam/main.tf
+++ serverless app/terraform/modules/iam/main.tf
@@ -9,4 +9,26 @@ data "aws_iam_policy_document" "this" {
       actions   = statement.value["actions"]
     }
   }
+
+  dynamic "statement" {
+    for_each = var.create_log_perms_for_lambda ? [
+      {
+        sid       = "AllowCreatingLogGroups"
+        resources = ["arn:aws:logs:*:*:*"]
+        actions   = ["logs:CreateLogGroup"]
+      },
+      {
+        sid       = "AllowWritingLogs"
+        resources = ["arn:aws:logs:*:*:log-group:/aws/lambda/*:*"]
+        actions   = ["logs:CreateLogStream", "logs:PutLogEvents"]
+      }
+    ] : []
+
+    content {
+      sid       = statement.value["sid"]
+      effect    = "Allow"
+      resources = statement.value["resources"]
+      actions   = statement.value["actions"]
+    }
+  }
 }
