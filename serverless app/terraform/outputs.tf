7ff7c520bf891df7521278a585f873e007e72e8a
Alves0611 <g.alves0611@gmail.com>
2024-08-11 12:48:57 -0300

    feat: add outputs for Lambda invoke URLs (S3, DynamoDB, SQS)

--- serverless app/terraform/outputs.tf
+++ serverless app/terraform/outputs.tf
@@ -9,3 +9,15 @@ output "cognito_client_id" {
 output "cognito_url" {
   value = "https://${aws_cognito_user_pool_domain.this.domain}.auth.${var.aws_region}.amazoncognito.com"
 }
+
+output "lambda_s3_invoke_url" {
+  value = module.lambda_s3.invoke_arn
+}
+
+output "lambda_dynamo_invoke_url" {
+  value = module.lambda_dynamodb.invoke_arn
+}
+
+output "lambda_sqs_invoke_url" {
+  value = module.lambda_sqs.invoke_arn
+}
