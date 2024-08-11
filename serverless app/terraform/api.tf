9f224d0034d687ba983003c9a055344d9789d396
Alves0611 <g.alves0611@gmail.com>
2024-08-11 12:36:28 -0300

    feat: add method and integration responses for CORS on 'todo/{todoId}' resource

--- serverless app/terraform/api.tf
+++ serverless app/terraform/api.tf
@@ -229,3 +229,33 @@ resource "aws_api_gateway_integration" "cors_todo" {
     "application/json" = "{\"statusCode\": 200}"
   }
 }
+
+resource "aws_api_gateway_method_response" "cors_todo" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  resource_id = aws_api_gateway_resource.todo.id
+  http_method = aws_api_gateway_method.cors_todo.http_method
+  status_code = "200"
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Credentials" = true
+    "method.response.header.Access-Control-Allow-Headers"     = false
+    "method.response.header.Access-Control-Allow-Methods"     = false
+    "method.response.header.Access-Control-Allow-Origin"      = false
+  }
+}
+
+resource "aws_api_gateway_integration_response" "cors_todo" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  resource_id = aws_api_gateway_resource.todo.id
+  http_method = aws_api_gateway_method.cors_todo.http_method
+  status_code = aws_api_gateway_method_response.cors_todo.status_code
+
+  response_parameters = {
+    "method.response.header.Access-Control-Allow-Headers"     = local.formatted_cors.headers
+    "method.response.header.Access-Control-Allow-Methods"     = local.formatted_cors.methods
+    "method.response.header.Access-Control-Allow-Origin"      = local.formatted_cors.origins
+    "method.response.header.Access-Control-Allow-Credentials" = local.formatted_cors.credentials
+  }
+
+  depends_on = [aws_api_gateway_integration.cors_todo]
+}
