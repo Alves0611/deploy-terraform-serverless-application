71b895f821e8f13069374a73f41761bdade3a927
Alves0611 <g.alves0611@gmail.com>
2024-08-11 13:33:58 -0300

    feat: create ACM certificate and DNS validation for API domain

--- serverless app/terraform/route53.tf
+++ serverless app/terraform/route53.tf
@@ -4,3 +4,17 @@ data "aws_route53_zone" "apex_domain" {
   name         = var.domain_name
   private_zone = false
 }
+
+resource "aws_acm_certificate" "api" {
+  count = local.create_resource_based_on_domain_name
+
+  domain_name       = "api.${var.domain_name}"
+  validation_method = "DNS"
+}
+
+resource "aws_acm_certificate_validation" "api" {
+  count = local.create_resource_based_on_domain_name
+
+  certificate_arn         = aws_acm_certificate.api[0].arn
+  validation_record_fqdns = [for record in aws_route53_record.api_validation : record.fqdn]
+}
