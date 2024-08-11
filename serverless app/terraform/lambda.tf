fa30aa84f5dcd7dbc7df9698053ec164cf9973c3
Alves0611 <g.alves0611@gmail.com>
2024-08-10 22:38:34 -0300

    feat: add Lambda module triggered by API Gateway to save data in DynamoDB

--- serverless app/terraform/lambda.tf
+++ serverless app/terraform/lambda.tf
@@ -45,3 +45,33 @@ module "lambda_s3" {
     DEBUG     = var.environment == "dev"
   }
 }
+
+module "lambda_dynamodb" {
+  source = "./modules/lambda"
+
+  name            = "${local.namespaced_service_name}-dynamodb"
+  description     = "Triggered by API Gateway to save data into DynamoDB table"
+  handler         = "${local.lambdas_path}/dynamodb.handler"
+  iam_role_arn    = module.iam_role_dynamodb_lambda.iam_role_arn
+  timeout_in_secs = 15
+  memory_in_mb    = 256
+  code_hash       = data.archive_file.codebase.output_base64sha256
+
+  s3_config = {
+    bucket = aws_s3_bucket.lambda_artefacts.bucket
+    key    = aws_s3_object.lambda_artefact.key
+  }
+
+  env_vars = {
+    JWT_SECRET   = aws_cognito_user_pool_client.this.id
+    TABLE_NAME   = aws_dynamodb_table.this.name
+    GSI_NAME     = local.dynamodb_config.gsi_name
+    DEBUG        = var.environment == "dev"
+    CORS_HEADERS = local.formatted_cors.headers
+    CORS_METHODS = local.formatted_cors.methods
+    CORS_ORIGINS = local.formatted_cors.origins
+    CORS_CREDS   = local.formatted_cors.credentials
+
+    AWS_NODEJS_CONNECTION_REUSE_ENABLED = 1
+  }
+}
