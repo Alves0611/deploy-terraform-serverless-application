f739231da2c55a5b03529855097afc88ebe7aa07
Alves0611 <g.alves0611@gmail.com>
2024-08-10 22:43:48 -0300

    feat: add IAM role module for DynamoDB Lambda with necessary permissions

--- serverless app/terraform/iam.tf
+++ serverless app/terraform/iam.tf
@@ -8,3 +8,33 @@ data "aws_iam_policy_document" "lambda_assume_role" {
     }
   }
 }
+
+module "iam_role_dynamodb_lambda" {
+  source = "./modules/iam"
+
+  iam_role_name   = "${local.namespaced_service_name}-dynamodb-lambda-role"
+  iam_policy_name = "${local.namespaced_service_name}-dynamodb-lambda-execute-policy"
+
+  assume_role_policy = data.aws_iam_policy_document.lambda_assume_role.json
+
+  create_log_perms_for_lambda = true
+
+  permissions = [
+    {
+      sid = "AllowDynamoDBActions"
+      actions = [
+        "dynamodb:BatchGetItem",
+        "dynamodb:BatchWriteItem",
+        "dynamodb:PutItem",
+        "dynamodb:DeleteItem",
+        "dynamodb:GetItem",
+        "dynamodb:Query",
+        "dynamodb:UpdateItem",
+      ]
+      resources = [
+        "arn:aws:dynamodb:${var.aws_region}:${local.account_id}:table/${aws_dynamodb_table.this.name}",
+        "arn:aws:dynamodb:${var.aws_region}:${local.account_id}:table/${aws_dynamodb_table.this.name}/index/*",
+      ]
+    }
+  ]
+}
