7a33a166143b884e3c6169f06870b189d423a8ea
Alves0611 <g.alves0611@gmail.com>
2024-08-11 12:29:43 -0300

    feat: add API Gateway methods with Cognito authorization for 'todos' and 'todo/{todoId}' paths

--- serverless app/terraform/api.tf
+++ serverless app/terraform/api.tf
@@ -30,3 +30,23 @@ resource "aws_api_gateway_resource" "todo" {
   parent_id   = aws_api_gateway_resource.todos.id
   path_part   = "{todoId}"
 }
+
+resource "aws_api_gateway_method" "todos" {
+  rest_api_id   = aws_api_gateway_rest_api.this.id
+  resource_id   = aws_api_gateway_resource.todos.id
+  authorization = "COGNITO_USER_POOLS"
+  http_method   = "ANY"
+  authorizer_id = aws_api_gateway_authorizer.this.id
+}
+
+resource "aws_api_gateway_method" "todo" {
+  rest_api_id   = aws_api_gateway_rest_api.this.id
+  resource_id   = aws_api_gateway_resource.todo.id
+  authorization = "COGNITO_USER_POOLS"
+  http_method   = "ANY"
+  authorizer_id = aws_api_gateway_authorizer.this.id
+
+  request_parameters = {
+    "method.request.path.todoId" = true
+  }
+}
