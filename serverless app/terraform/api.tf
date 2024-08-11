37ed7750e3ddd3dbe68909a2fa418c18aff39e82
Alves0611 <g.alves0611@gmail.com>
2024-08-11 12:29:11 -0300

    feat: add API Gateway authorizer and resources for 'v1/todos/{todoId}' path

--- serverless app/terraform/api.tf
+++ serverless app/terraform/api.tf
@@ -5,3 +5,28 @@ resource "aws_api_gateway_rest_api" "this" {
     types = ["REGIONAL"]
   }
 }
+
+resource "aws_api_gateway_authorizer" "this" {
+  rest_api_id   = aws_api_gateway_rest_api.this.id
+  name          = "CognitoUserPoolAuthorizer"
+  type          = "COGNITO_USER_POOLS"
+  provider_arns = [aws_cognito_user_pool.this.arn]
+}
+
+resource "aws_api_gateway_resource" "v1" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  parent_id   = aws_api_gateway_rest_api.this.root_resource_id
+  path_part   = "v1"
+}
+
+resource "aws_api_gateway_resource" "todos" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  parent_id   = aws_api_gateway_resource.v1.id
+  path_part   = "todos"
+}
+
+resource "aws_api_gateway_resource" "todo" {
+  rest_api_id = aws_api_gateway_rest_api.this.id
+  parent_id   = aws_api_gateway_resource.todos.id
+  path_part   = "{todoId}"
+}
