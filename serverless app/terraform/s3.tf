0dda1d69211005c6c9ecc0fb5b180315d7afe350
Alves0611 <g.alves0611@gmail.com>
2024-08-11 12:51:15 -0300

    feat: create S3 bucket with random name and Lambda trigger for object creation

--- serverless app/terraform/s3.tf
+++ serverless app/terraform/s3.tf
@@ -1,3 +1,18 @@
+resource "random_pet" "bucket" {}
+
+resource "aws_s3_bucket" "todo" {
+  bucket = "${local.namespaced_service_name}-${random_pet.bucket.id}"
+}
+
+resource "aws_s3_bucket_notification" "lambda" {
+  bucket = aws_s3_bucket.todo.id
+
+  lambda_function {
+    lambda_function_arn = module.lambda_s3.arn
+    events              = ["s3:ObjectCreated:*"]
+  }
+}
+
 resource "aws_s3_bucket" "lambda_artefacts" {
   bucket = "${local.account_id}-lambda-artefacts"
 }
