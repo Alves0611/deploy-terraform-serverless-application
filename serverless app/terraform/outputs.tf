c8231747f01fe234df5b21dff14c8192bb76a232
Alves0611 <g.alves0611@gmail.com>
2024-08-11 13:45:55 -0300

    feat: add outputs for API custom domain URL and S3 bucket name

--- serverless app/terraform/outputs.tf
+++ serverless app/terraform/outputs.tf
@@ -29,3 +29,11 @@ output "api_url" {
 output "bucket_name_lambda_artefacts" {
   value = aws_s3_bucket.lambda_artefacts.bucket
 }
+
+output "api_custom_domain_url" {
+  value = local.has_domain_name ? "https://${aws_api_gateway_domain_name.this[0].domain_name}" : ""
+}
+
+output "bucket_name_todo" {
+  value = aws_s3_bucket.todo.bucket
+}
