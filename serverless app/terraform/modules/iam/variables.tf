0b04c47d86c5e5ad59fb60f904283467900101e8
Alves0611 <g.alves0611@gmail.com>
2024-08-10 22:21:36 -0300

    feat: add variables for IAM role name, policy name, and assume role policy

--- serverless app/terraform/modules/iam/variables.tf
+++ serverless app/terraform/modules/iam/variables.tf
@@ -13,3 +13,28 @@ variable "create_log_perms_for_lambda" {
   description = "Whether to create the Lambda permissions for Cloudwatch"
   default     = false
 }
+
+variable "iam_role_name" {
+  type        = string
+  description = "The name of the IAM role"
+
+  validation {
+    condition     = can(regex("^[a-zA-Z0-9+=,.@_-]{1,64}$", var.iam_role_name))
+    error_message = "Invalid IAM role name. It must adhere to the naming rules."
+  }
+}
+
+variable "iam_policy_name" {
+  type        = string
+  description = "The name of the IAM policy"
+
+  validation {
+    condition     = can(regex("^[a-zA-Z0-9+=,.@_ -]{1,128}$", var.iam_policy_name))
+    error_message = "Invalid IAM policy name. It must adhere to the naming rules."
+  }
+}
+
+variable "assume_role_policy" {
+  type        = string
+  description = "The assume role policy in JSON format"
+}
