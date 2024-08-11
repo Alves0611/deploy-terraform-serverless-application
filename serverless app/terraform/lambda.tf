02486775046235a1dccb09ed2afde4f94f5f8343
Alves0611 <g.alves0611@gmail.com>
2024-08-10 22:38:10 -0300

    feat: add Lambda module for processing S3 files and publishing to SNS

--- serverless app/terraform/lambda.tf
+++ serverless app/terraform/lambda.tf
@@ -23,3 +23,25 @@ data "archive_file" "codebase" {
   source_dir  = "${path.module}/../dist"
   output_path = "files/${random_uuid.build_id.result}.zip"
 }
+
+module "lambda_s3" {
+  source = "./modules/lambda"
+
+  name            = "${local.namespaced_service_name}-s3"
+  description     = "Reads file from S3 and publishes messages into a SNS topic"
+  iam_role_arn    = module.iam_role_s3_lambda.iam_role_arn
+  handler         = "${local.lambdas_path}/s3.handler"
+  timeout_in_secs = 20
+  memory_in_mb    = 256
+  code_hash       = data.archive_file.codebase.output_base64sha256
+
+  s3_config = {
+    bucket = aws_s3_bucket.lambda_artefacts.bucket
+    key    = aws_s3_object.lambda_artefact.key
+  }
+
+  env_vars = {
+    TOPIC_ARN = aws_sns_topic.this.arn
+    DEBUG     = var.environment == "dev"
+  }
+}
