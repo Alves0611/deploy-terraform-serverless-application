36d7cbd493827908be427fff81f8e10e4eab2824
Alves0611 <g.alves0611@gmail.com>
2024-08-11 13:34:38 -0300

    feat: create Route 53 A record for API domain pointing to API Gateway

--- serverless app/terraform/route53.tf
+++ serverless app/terraform/route53.tf
@@ -35,3 +35,17 @@ resource "aws_route53_record" "api_validation" {
   type            = each.value.type
   zone_id         = data.aws_route53_zone.apex_domain[0].zone_id
 }
+
+resource "aws_route53_record" "api" {
+  count = local.create_resource_based_on_domain_name
+
+  name    = aws_api_gateway_domain_name.this[0].domain_name
+  type    = "A"
+  zone_id = data.aws_route53_zone.apex_domain[0].zone_id
+
+  alias {
+    evaluate_target_health = true
+    name                   = aws_api_gateway_domain_name.this[0].regional_domain_name
+    zone_id                = aws_api_gateway_domain_name.this[0].regional_zone_id
+  }
+}
