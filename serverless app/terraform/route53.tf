85282228b7499c53dd05801e4b9beca31b2b5709
Alves0611 <g.alves0611@gmail.com>
2024-08-11 13:34:22 -0300

    feat: add Route 53 records for DNS validation of API ACM certificate

--- serverless app/terraform/route53.tf
+++ serverless app/terraform/route53.tf
@@ -18,3 +18,20 @@ resource "aws_acm_certificate_validation" "api" {
   certificate_arn         = aws_acm_certificate.api[0].arn
   validation_record_fqdns = [for record in aws_route53_record.api_validation : record.fqdn]
 }
+
+resource "aws_route53_record" "api_validation" {
+  for_each = local.has_domain_name ? {
+    for dvo in aws_acm_certificate.api[0].domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  } : {}
+
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = data.aws_route53_zone.apex_domain[0].zone_id
+}
